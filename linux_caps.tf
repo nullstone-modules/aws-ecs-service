2a3734f390f1ef1dd7a3962ce078d0b6b664f0a2
Brad Sickles <brad.sickles@gmail.com>
2025-08-18 14:36:04 -0400

    Added support for `linuxParameters` in capabilities. (#6)

--- linux_caps.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  all_kernel_cap_add = [for kc in lookup(local.capabilities, "cap_add", []) : try(kc.name, "")]
-  kernel_cap_add     = length(local.all_kernel_cap_add) > 0 ? coalescelist(local.all_kernel_cap_add) : []
-}
